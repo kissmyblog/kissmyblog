b6871df979bd414cc204e5d212cbff3e85f97634
Jean-Paul Bonnetouche <goodtouch@gmail.com>
2014-10-14 01:42:16 +0200

    Add sample config files

---
+++ lib/support/deploy/deploy.sh
@@ -0,0 +1,38 @@
+# This is deploy script we use to update our production server
+# You can always modify it for your needs :)
+
+# If any command return non-zero status - stop deploy
+set -e
+
+echo 'Deploy: Show deploy index page'
+cp ~/kissmyblog/public/deploy.html ~/kissmyblog/public/index.html
+
+echo 'Deploy: Stop KISSmyBLOG server'
+/etc/init.d/kissmyblog stop
+
+echo 'Deploy: Get latest code'
+cd ~/kissmyblog
+
+# clean working directory
+# git stash
+
+# change branch to
+git pull origin master
+
+echo 'Deploy: Bundle & Compile assets'
+
+# change it to your needs
+bundle --without development test --deployment
+
+bundle exec rake assets:clean RAILS_ENV=production
+bundle exec rake assets:precompile RAILS_ENV=production
+bundle exec rake cache:clear RAILS_ENV=production
+
+# return stashed changes (if necessary)
+# git stash pop
+
+echo 'Deploy: Starting KISSmyBLOG server...'
+/etc/init.d/kissmyblog start
+
+rm ~/kissmyblog/public/index.html
+echo 'Deploy: Done'
